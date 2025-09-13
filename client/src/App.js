8e124125d8268b2f9c8da7ed9eedfe90fab5837a
meztinosdunken <orlanmorry5462@hotmail.com>
2025-09-13 09:54:21 -0500

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Agile Metrics Dashboard</h1>
+      <p>Visualize your Agile metrics.</p>
+    </div>
+  );
+}
+
+export default App;
